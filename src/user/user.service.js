0b5bff79494bb8f1e13c075520bd2c6485065469
Thanh Trung Nguyen <thanh.nguyen-trung@hellopay.com>
2016-12-05 17:54:49 +0700

    module 5

---
+++ src/user/user.service.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('user')
+    .service('UserService', UserService);
+
+    UserService.$inject = ["$q"];
+    function UserService($q) {
+        var service = this;
+        var user_info = {};
+        var has_registered = false;
+
+        service.RegisterUser = function (info) {
+            var def = $q.defer();
+            user_info = info;
+            has_registered = true;
+            def.resolve(has_registered);
+            return def.promise;
+        };
+
+        service.GetUser = function () {
+            var def = $q.defer();
+            def.resolve(user_info);
+            return def.promise;
+        };
+
+        service.IsRegistered = function () {
+            var def = $q.defer();
+            def.resolve(has_registered);
+            return def.promise;
+        };
+    }
+})();
